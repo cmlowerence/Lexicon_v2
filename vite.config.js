e9742284fb37cc8105290fe2c166cfd154d8c4be
cmlowerence <cmlowerence2777@gmail.com>
2026-04-05 19:53:18 +0530

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: import.meta.env.VITE_APP_BASE || '/',
+  base: process.env.VITE_APP_BASE || '/',
   plugins: [react()],
   test: {
     globals: true,
